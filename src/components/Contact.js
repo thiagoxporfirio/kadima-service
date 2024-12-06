eae11b68e2e30a19ddc2ed393399f485aa137cb0
thiagoxporfirio <thiagovporfirio@gmail.com>
2024-12-06 00:01:02 -0300

    fix: remove no used

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 import {
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,5 @@
 import React from "react";
 import {
-	FaInstagram,
-	FaYoutube,
-	FaTiktok,
 	FaLinkedin,
 	FaFacebook
 } from "react-icons/fa";
